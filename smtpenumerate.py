80df6106232df13b8c17354ab70963d9807c3a96
Jonathan Schramm <JonathanSchramm91@gmail.com>
2015-07-12 15:35:24 +0200

    Create smtpenumerate.py

---
+++ smtpenumerate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import socket
+ 
+if(len(sys.argv) < 3):
+        print("usage: SMTPenumerator.py userlist IP")
+        sys.exit()
+ 
+wordFile = open(sys.argv[1], "r")
+foundUsers = []
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((sys.argv[2], 25))
+s.send("HELO\n")
+s.recv(1024)
+s.send("MAIL from:<>\n")
+s.recv(1024)
+ 
+for line in wordFile:
+        s.send("RCPT to:" + line)
+        response = s.recv(1024)
+        if "Recipient ok" in response:
+                foundUsers.append(line)
+ 
+print("Users found: " + str(len(foundUsers)))
+for user in foundUsers:
+        print(user.rstrip())
